4809a66d6da90da7d47442a94a1cd9a93a85dedd
Olena <86402764+mikhailova11@users.noreply.github.com>
2022-06-24 22:00:37 +0300

    hw06-email

---
+++ services/email.service.js
@@ -0,0 +1,31 @@
+const sgMail = require('@sendgrid/mail');
+const {SEND_GRID_API_KEY, PORT} =  require('../env')
+
+const BASE_URL = `http://localhost:${PORT}/api`
+
+const sendEmail = async (userEmail, code)  => {
+    sgMail.setApiKey(SEND_GRID_API_KEY);
+     const link = `${BASE_URL}/auth/verify/${code}`
+    
+    const msg = {
+        to: userEmail,
+        from: 'rtvr@ukr.net',
+        subject: 'Confirm your email',
+        html: `<h4>Click on this link to confirm registration ${link}</h4>`,
+    }
+    
+    try {
+        const result = await sgMail.send(msg);
+        console.log('result', result);
+    
+    } catch (e) {
+        console.log('ERROR', e);
+        throw e;
+    }
+
+}
+
+module.exports = {
+    sendEmail
+}
+
